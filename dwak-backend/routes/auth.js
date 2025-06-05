4b497b2ae1747d08fab6d4c47e72fc7193eac037
tchenirou <srafik46@gmail.com>
2025-06-06 00:53:40 +0100

    Update auth.js

--- dwak-backend/routes/auth.js
+++ dwak-backend/routes/auth.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 
 // POST /api/auth/login
 router.post('/login', async (req, res) => {
